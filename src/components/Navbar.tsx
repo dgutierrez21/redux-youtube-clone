2763bc2ceaf8989a0e3920355bd79d278c44f521
Dixon Gutiérrez <96222189+dgutierrez21@users.noreply.github.com>
2023-05-20 21:34:32 -0600

    add search form to navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,7 @@
 import { GiHamburgerMenu } from "react-icons/gi";
 import { BsYoutube } from "react-icons/bs";
+import { AiOutlineSearch, AiOutlineClose } from "react-icons/ai";
+
 import { Link } from "react-router-dom";
 
 export const Navbar = () => {
@@ -15,6 +17,28 @@ export const Navbar = () => {
             <span className="text-xl font-medium">YouTube</span>
           </div>
         </Link>
+      </div>
+
+      <div className="flex items-center justify-center gap-5">
+        <form action="">
+          <div className="flex h-10 items-center bg-zinc-900 px-4 pr-0">
+            <div className="flex items-center gap-4 pr-5">
+              <div>
+                <AiOutlineSearch className="text-xl" />
+              </div>
+
+              <input
+                type="text"
+                className="w-96 border-none bg-zinc-900 focus:outline-none"
+              />
+
+              <AiOutlineClose className="cursor-pointer text-xl" />
+            </div>
+            <button className="flex h-10 w-16 items-center justify-center bg-zinc-800">
+              <AiOutlineSearch className="text-xl" />
+            </button>
+          </div>
+        </form>
       </div>
     </div>
   );
