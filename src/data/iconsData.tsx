ab1859f6e02f98e2e4515acf171e2a38cfee3d23
Dixon Gutiérrez <96222189+dgutierrez21@users.noreply.github.com>
2023-05-21 12:35:10 -0600

    add icons data

---
+++ src/data/iconsData.tsx
@@ -0,0 +1,124 @@
+import { ReactElement } from "react";
+
+import {
+  MdHomeFilled,
+  MdOutlineSlowMotionVideo,
+  MdSubscriptions,
+  MdOutlineVideoLibrary,
+  MdHistory,
+  MdOutlineSmartDisplay,
+  MdOutlineWatchLater,
+  MdThumbUpOffAlt,
+  MdSettings,
+  MdOutlinedFlag,
+  MdOutlineHelpOutline,
+  MdOutlineFeedback,
+  MdOutlineSportsVolleyball,
+} from "react-icons/md";
+import { TbMusic, TbDeviceGamepad2 } from "react-icons/tb";
+import { FaRegCompass } from "react-icons/fa";
+import { GiFilmStrip } from "react-icons/gi";
+
+interface Icon {
+  icon: ReactElement;
+  name: string;
+}
+
+export const mainLinks: Icon[] = [
+  {
+    icon: <MdHomeFilled className="text-xl" />,
+    name: "Home",
+  },
+  {
+    icon: <FaRegCompass className="text-xl" />,
+    name: "Explore",
+  },
+  {
+    icon: <MdOutlineSlowMotionVideo className="text-xl" />,
+    name: "Shorts",
+  },
+  {
+    icon: <MdSubscriptions className="text-xl" />,
+    name: "Subscriptions",
+  },
+];
+
+export const secondaryLinks: Icon[] = [
+  {
+    icon: <MdOutlineVideoLibrary className="text-xl" />,
+    name: "Library",
+  },
+  {
+    icon: <MdHistory className="text-xl" />,
+    name: "History",
+  },
+  {
+    icon: <MdOutlineSmartDisplay className="text-xl" />,
+    name: "Your Videos",
+  },
+  {
+    icon: <MdOutlineWatchLater className="text-xl" />,
+    name: "Watch Later",
+  },
+  {
+    icon: <MdThumbUpOffAlt className="text-xl" />,
+    name: "Liked Videos",
+  },
+];
+
+export const subscriptionLinks: Icon[] = [
+  {
+    icon: <TbMusic className="text-xl" />,
+    name: "Music",
+  },
+  {
+    icon: <MdOutlineSportsVolleyball className="text-xl" />,
+    name: "Sport",
+  },
+  {
+    icon: <TbDeviceGamepad2 className="text-xl" />,
+    name: "Gaming",
+  },
+  {
+    icon: <GiFilmStrip className="text-xl" />,
+    name: "Films",
+  },
+];
+
+export const helpsLinks: Icon[] = [
+  {
+    icon: <MdSettings className="text-xl" />,
+    name: "Settings",
+  },
+  {
+    icon: <MdOutlinedFlag className="text-xl" />,
+    name: "Report history",
+  },
+  {
+    icon: <MdOutlineHelpOutline className="text-xl" />,
+    name: "Help",
+  },
+  {
+    icon: <MdOutlineFeedback className="text-xl" />,
+    name: "Send feedback",
+  },
+];
+
+export const textLinks: string[][] = [
+  [
+    "About",
+    "Press",
+    "Copyright",
+    "Contact us",
+    "Creator",
+    "Advertise",
+    "Developers",
+  ],
+  [
+    "Terms",
+    "Privacy",
+    "Policy & Safety",
+    "How YouTube works",
+    "Test new features",
+  ],
+];
