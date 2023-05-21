e7e057e0f839b2d4fca23a52df185066fa61f7ac
Dixon Gutiérrez <96222189+dgutierrez21@users.noreply.github.com>
2023-05-21 11:55:07 -0600

    add styles to the home component to add sidebar

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,8 +2,11 @@ import { Navbar } from "../components/Navbar";
 
 export const Home = () => {
   return (
-    <div>
-      <Navbar />
+    <div className="max-h-screen overflow-hidden">
+      <div style={{ height: "7.5vh" }}>
+        <Navbar />
+      </div>
+      <div className="flex" style={{ height: "92.5vh" }}></div>
     </div>
   );
 };
