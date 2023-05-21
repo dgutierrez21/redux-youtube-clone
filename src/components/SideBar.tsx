e7e73d83c78b2669e2d549432b7ec12824488439
Dixon Gutiérrez <96222189+dgutierrez21@users.noreply.github.com>
2023-05-21 14:36:48 -0600

    add main links to the sidebar

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,5 +1,23 @@
+import { mainLinks } from "../data/iconsData";
+
 export const SideBar = () => {
   return (
-    <div className="sidebar w-2/12 overflow-auto bg-[#212121] pb-8 pr-5"></div>
+    <div className="sidebar w-2/12 overflow-auto bg-[#212121] pb-8 pr-5">
+      <ul className="flex flex-col border-b-2 border-gray-700">
+        {mainLinks.map(({ icon, name }) => (
+          <li
+            className={`py-3 pl-6 hover:bg-zinc-600 ${
+              name === "Home" ? "bg-slate-600" : ""
+            }`}
+            key={name}
+          >
+            <a href="#" className="flex items-center gap-5">
+              {icon}
+              <span className="text-sm tracking-wider">{name}</span>
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 };
