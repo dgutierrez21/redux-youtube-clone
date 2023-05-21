52436e4ed2a2949757a9d17a04c9015d6d6c08a1
Dixon Gutiérrez <96222189+dgutierrez21@users.noreply.github.com>
2023-05-20 20:54:06 -0600

    router setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,30 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
+import { Home } from "./pages/Home";
+import { Search } from "./pages/Search";
+import { Watch } from "./pages/Watch";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/search",
+    element: <Search />,
+  },
+  {
+    path: "/watch/:id",
+    element: <Watch />,
+  },
+]);
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
 export default App;
