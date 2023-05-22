44df9e3f3af4619d5f4c0216cf6c97181dadd6af
Dixon Gutiérrez <96222189+dgutierrez21@users.noreply.github.com>
2023-05-22 06:53:21 -0600

    create store

---
+++ src/store/index.ts
@@ -0,0 +1,12 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { YoutubeSlice } from "./slices/youtubeSlice";
+
+export const store = configureStore({
+  reducer: {
+    youtubeApp: YoutubeSlice.reducer,
+  },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+
+export type AppDispacth = typeof store.dispatch;
