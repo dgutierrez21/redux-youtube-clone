2c1123824822ab7ae55de98600e360523a270cc4
Dixon Gutiérrez <96222189+dgutierrez21@users.noreply.github.com>
2023-05-21 19:12:34 -0600

    add helps links to the sidebar

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,4 +1,5 @@
 import {
+  helpsLinks,
   mainLinks,
   secondaryLinks,
   subscriptionLinks,
@@ -36,6 +37,17 @@ export const SideBar = () => {
 
       <ul className="flex flex-col border-b-2 border-gray-700">
         {subscriptionLinks.map(({ icon, name }) => (
+          <li className="py-3 pl-6 hover:bg-zinc-600" key={name}>
+            <a href="#" className="flex items-center gap-5">
+              {icon}
+              <span className="text-sm tracking-wider">{name}</span>
+            </a>
+          </li>
+        ))}
+      </ul>
+
+      <ul className="flex flex-col border-b-2 border-gray-700">
+        {helpsLinks.map(({ icon, name }) => (
           <li className="py-3 pl-6 hover:bg-zinc-600" key={name}>
             <a href="#" className="flex items-center gap-5">
               {icon}
