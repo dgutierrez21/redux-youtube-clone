4a641552f7fa6ef747d418c417da953092ad23d5
Dixon Gutiérrez <96222189+dgutierrez21@users.noreply.github.com>
2023-05-22 11:25:14 -0600

    Fix import name of type "AppDispacth"

--- src/store/hooks.ts
+++ src/store/hooks.ts
@@ -1,9 +1,9 @@
 import { useDispatch, useSelector } from "react-redux";
-import { AppDispacth, RootState } from "./index";
+import { AppDispatch, RootState } from "./index";
 
 import { TypedUseSelectorHook } from "react-redux/es/types";
 
-export const useAppDispatch: () => AppDispacth = useDispatch;
+export const useAppDispatch: () => AppDispatch = useDispatch;
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 
 // Aunque es posible importar los tipos RootState y AppDispatch en cada componente, es mejor crear versiones tipadas de los hooks useDispatch y useSelector para su uso en la aplicación. Esto es importante por un par de razones:
