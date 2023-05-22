20c4d8d3e4124213e2abe8cada2e34d1f6797f97
Dixon Gutiérrez <96222189+dgutierrez21@users.noreply.github.com>
2023-05-21 19:12:13 -0600

    add subscription links to the sidebar

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,4 +1,8 @@
-import { mainLinks, secondaryLinks } from "../data/iconsData";
+import {
+  mainLinks,
+  secondaryLinks,
+  subscriptionLinks,
+} from "../data/iconsData";
 
 export const SideBar = () => {
   return (
@@ -21,6 +25,17 @@ export const SideBar = () => {
 
       <ul className="flex flex-col border-b-2 border-gray-700">
         {secondaryLinks.map(({ icon, name }) => (
+          <li className="py-3 pl-6 hover:bg-zinc-600" key={name}>
+            <a href="#" className="flex items-center gap-5">
+              {icon}
+              <span className="text-sm tracking-wider">{name}</span>
+            </a>
+          </li>
+        ))}
+      </ul>
+
+      <ul className="flex flex-col border-b-2 border-gray-700">
+        {subscriptionLinks.map(({ icon, name }) => (
           <li className="py-3 pl-6 hover:bg-zinc-600" key={name}>
             <a href="#" className="flex items-center gap-5">
               {icon}
