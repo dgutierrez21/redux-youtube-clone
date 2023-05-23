5dc9730b6e054e9cb6fc1899352c5306af66b647
Dixon Gutiérrez <96222189+dgutierrez21@users.noreply.github.com>
2023-05-23 09:32:20 -0600

    create spinner

---
+++ src/components/Spinner.tsx
@@ -0,0 +1,7 @@
+export const Spinner = () => {
+  return (
+    <div className="flex w-full items-center justify-center py-3">
+      <div className="h-10 w-10 animate-spin rounded-full border-2 border-solid border-red-600 border-t-transparent"></div>
+    </div>
+  );
+};
