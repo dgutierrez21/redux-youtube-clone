ae2fdf9adfd879d05b9febae1966e5df5364201d
Dixon Gutiérrez <96222189+dgutierrez21@users.noreply.github.com>
2023-05-23 10:08:44 -0600

    show video duration

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,10 +1,18 @@
 import { Link } from "react-router-dom";
 import { HomePageVideos } from "../Types";
 
-export const Card = ({ videoThumbnail, videoId }: HomePageVideos) => {
+export const Card = ({
+  videoDuration,
+  videoThumbnail,
+  videoId,
+}: HomePageVideos) => {
   return (
     <div className="flex h-60 w-64 flex-col gap-3">
-      <div>
+      <div className="relative">
+        <span className="absolute bottom-3 right-3 z-10 bg-gray-900 px-2 py-0.5 text-sm">
+          {videoDuration}
+        </span>
+
         <Link to={`/watch/${videoId}`}>
           <img src={videoThumbnail} alt="thumbnail" className="h-44 w-72" />
         </Link>
