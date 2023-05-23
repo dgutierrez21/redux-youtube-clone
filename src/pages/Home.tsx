ae5bc7000032203f6505639e4060a5c0eb49d9f9
Dixon Gutiérrez <96222189+dgutierrez21@users.noreply.github.com>
2023-05-23 09:46:58 -0600

    conditionally display spinner and card with infiniteScroll

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,17 @@
 import { useEffect } from "react";
 import { Navbar } from "../components/Navbar";
 import { SideBar } from "../components/SideBar";
-import { useAppDispatch } from "../store/hooks";
+import { useAppDispatch, useAppSelector } from "../store/hooks";
 import { getHomePageVideos } from "../store/reducers/youtubeSliceReducers/getHomePageVideos";
+
+import InfiniteScroll from "react-infinite-scroll-component";
+import { Spinner } from "../components/Spinner";
+import { HomePageVideos } from "../Types";
+import { Card } from "../components/Card";
 
 export const Home = () => {
   const dispatch = useAppDispatch();
+  const videos = useAppSelector((state) => state.youtubeApp.videos);
 
   useEffect(() => {
     dispatch(getHomePageVideos(false));
@@ -18,6 +24,24 @@ export const Home = () => {
       </div>
       <div className="flex" style={{ height: "92.5vh" }}>
         <SideBar />
+
+        {videos.length ? (
+          <InfiniteScroll
+            dataLength={videos.length}
+            next={() => dispatch(getHomePageVideos(true))}
+            hasMore={videos.length < 500}
+            loader={<Spinner />}
+            height={650}
+          >
+            <div className="grid grid-cols-4 gap-x-8 gap-y-14 p-8">
+              {videos.map((item: HomePageVideos) => (
+                <Card key={item.videoId} {...item} />
+              ))}
+            </div>
+          </InfiniteScroll>
+        ) : (
+          <Spinner />
+        )}
       </div>
     </div>
   );
