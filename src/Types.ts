6159f61e9d92852f8b4c9d05126d9dd51aec5375
Dixon Gutiérrez <96222189+dgutierrez21@users.noreply.github.com>
2023-05-22 18:51:21 -0600

    define HomePageVideos interface

--- src/Types.ts
+++ src/Types.ts
@@ -1,4 +1,18 @@
-export interface HomePageVideos {}
+export interface HomePageVideos {
+  videoId: string;
+  videoTitle: string;
+  videoDescription: string;
+  videoThumbnail: string;
+  videoLink: string;
+  videoDuration: string;
+  videoViews: string;
+  videoAge: string;
+  channelInfo: {
+    id: string;
+    image: string;
+    name: string;
+  };
+}
 export interface CurrentPlaying {}
 export interface RecommendedVideos {}
 
