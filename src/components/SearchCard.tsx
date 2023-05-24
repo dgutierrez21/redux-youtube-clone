f7d48cef6431f590ad5f099fb9889af8ab7184a9
Dixon Gutiérrez <96222189+dgutierrez21@users.noreply.github.com>
2023-05-24 15:40:38 -0600

    add video duration to SearchCard

--- src/components/SearchCard.tsx
+++ src/components/SearchCard.tsx
@@ -2,12 +2,17 @@ import { Link } from "react-router-dom";
 import { HomePageVideos } from "../Types";
 
 export const SearchCard = ({
+  videoDuration,
   videoThumbnail,
   videoId,
 }: HomePageVideos) => {
   return (
     <div className="flex gap-3">
       <div className="relative">
+        <span className="absolute bottom-3 right-3 z-10 bg-gray-900 px-2 py-0.5 text-sm">
+          {videoDuration}
+        </span>
+
         <Link to={`/watch/${videoId}`}>
           <img src={videoThumbnail} alt="thumbnail" className="h-52 w-96" />
         </Link>
