3999becec0f5e186ebaae3fa66983899b75fabfd
Dixon Gutiérrez <96222189+dgutierrez21@users.noreply.github.com>
2023-05-24 12:36:19 -0600

    show search page videos

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,3 +1,55 @@
+import { useEffect } from "react";
+import { Navbar } from "../components/Navbar";
+import { SideBar } from "../components/SideBar";
+import { useAppDispatch, useAppSelector } from "../store/hooks";
+
+import InfiniteScroll from "react-infinite-scroll-component";
+import { Spinner } from "../components/Spinner";
+import { HomePageVideos } from "../Types";
+import { Card } from "../components/Card";
+import { getSearchPageVideos } from "../store/reducers/youtubeSliceReducers/getSearchPageVideos";
+import { useNavigate } from "react-router-dom";
+
 export const Search = () => {
-  return <div>Search</div>;
+  const navigate = useNavigate();
+
+  const dispatch = useAppDispatch();
+  const videos = useAppSelector((state) => state.youtubeApp.videos);
+  const searchTerm = useAppSelector((state) => state.youtubeApp.searchTerm);
+
+  useEffect(() => {
+    if (!searchTerm) {
+      navigate("/");
+      return;
+    }
+  }, [searchTerm, navigate]);
+
+  return (
+    <div className="max-h-screen overflow-hidden">
+      <div style={{ height: "7.5vh" }}>
+        <Navbar />
+      </div>
+      <div className="flex" style={{ height: "92.5vh" }}>
+        <SideBar />
+
+        {videos.length ? (
+          <InfiniteScroll
+            dataLength={videos.length}
+            next={() => dispatch(getSearchPageVideos(true))}
+            hasMore={videos.length < 500}
+            loader={<Spinner />}
+            height={600}
+          >
+            <div className="grid grid-cols-4 gap-x-8 gap-y-14 p-8">
+              {videos.map((item: HomePageVideos) => (
+                <Card key={item.videoId} {...item} />
+              ))}
+            </div>
+          </InfiniteScroll>
+        ) : (
+          <Spinner />
+        )}
+      </div>
+    </div>
+  );
 };
