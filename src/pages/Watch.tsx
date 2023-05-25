1b311bbdf83d5b4c05a7462f6cbd57615041e1c9
Dixon Gutiérrez <96222189+dgutierrez21@users.noreply.github.com>
2023-05-24 20:26:29 -0600

    execute actions if id exists or not

--- src/pages/Watch.tsx
+++ src/pages/Watch.tsx
@@ -1,3 +1,23 @@
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { useAppDispatch } from "../store/hooks";
+
 export const Watch = () => {
+  const [showMoreStatus, setShowMoreStatus] = useState<boolean>(false);
+
+  const { id } = useParams();
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (id) {
+      dispatch(getVideoDetails(id));
+      setShowMoreStatus(false);
+      return;
+    }
+
+    navigate("/");
+  }, [dispatch, id, navigate]);
+
   return <div>Watch</div>;
 };
