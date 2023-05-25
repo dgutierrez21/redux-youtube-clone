d7770ad87c79c7d7676b8e4d3e3a228ffb9ce2dd
Dixon Gutiérrez <96222189+dgutierrez21@users.noreply.github.com>
2023-05-25 08:20:17 -0600

    add navbar to watch page

--- src/pages/Watch.tsx
+++ src/pages/Watch.tsx
@@ -3,6 +3,7 @@ import { useNavigate, useParams } from "react-router-dom";
 import { useAppDispatch, useAppSelector } from "../store/hooks";
 import { getVideoDetails } from "../store/reducers/youtubeSliceReducers/getVideoDetails";
 import { getRecommendedVideos } from "../store/reducers/youtubeSliceReducers/getRecommendedVideos";
+import { Navbar } from "../components/Navbar";
 
 export const Watch = () => {
   const [showMoreStatus, setShowMoreStatus] = useState<boolean>(false);
@@ -29,5 +30,15 @@ export const Watch = () => {
     if (currentPlaying && id) dispatch(getRecommendedVideos(id));
   }, [currentPlaying, id, dispatch]);
 
-  return <div>Watch</div>;
+  return (
+    <>
+      {currentPlaying && currentPlaying?.videoId === id && (
+        <div className="max-h-scren overflow-hidden">
+          <div className="" style={{ height: "7.5vh" }}>
+            <Navbar />
+          </div>
+        </div>
+      )}
+    </>
+  );
 };
