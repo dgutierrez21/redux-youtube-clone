2c729854e03927372dd375b917170d3498afb751
Dixon Gutiérrez <96222189+dgutierrez21@users.noreply.github.com>
2023-05-25 08:37:51 -0600

    add iframe to display video

--- src/pages/Watch.tsx
+++ src/pages/Watch.tsx
@@ -37,6 +37,25 @@ export const Watch = () => {
           <div className="" style={{ height: "7.5vh" }}>
             <Navbar />
           </div>
+
+          <div style={{ height: "92.5vh" }} className="flex w-full">
+            <div className="ml-20 mr-0 flex w-full gap-x-5 gap-y-10 overflow-auto p-7">
+              <div className="" style={{ maxWidth: "800px" }}>
+                <div>
+                  <iframe
+                    src={`https://www.youtube.com/embed/${id}?autoplay-1`}
+                    style={{
+                      border: "1px solid black",
+                    }}
+                    width="800"
+                    height="500"
+                    allowFullScreen
+                    title="Youtube video player"
+                  ></iframe>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       )}
     </>
